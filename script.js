dbd7c821171cfb5ae7546e189e137c2210f3266c
Lockeheede <prescott.lawani@gmail.com>
2020-03-21 03:31:40 -0500

    Completed the html, css and started the js code.

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const showSelect = document.getElementById('show');
+
+const ticketPrice = +showSelect.value;
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+  }
+});
