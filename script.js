f33d835208b5fdc051f712d0963d39e02534f3bd
Lockeheede <prescott.lawani@gmail.com>
2020-03-21 22:58:12 -0500

    Finished the project; the seats, total, tickets stay updated when you reload.

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const showSelect = document.getElementById('show');
 
+populateUI();
+
 let ticketPrice = +showSelect.value;
+
+//Save selected show index and price
+function setShowData(showIndex, showPrice) {
+  localStorage.setItem('selectedShowIndex', showIndex);
+  localStorage.setItem('selectedShowPrice', showPrice);
+}
 
 //Update total and counter
 function updateSelectedCount() {
@@ -12,13 +20,41 @@ function updateSelectedCount() {
 
   const selectedSeatsCount = selectedSeats.length;
 
+  //Copy selected seats into arr
+  //Map through array
+  //Return a new array of indexes
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedShowIndex = localStorage.getItem('selectedShowIndex');
+
+  if (selectedShowIndex !== null) {
+    showSelect.selectedIndex = selectedShowIndex;
+  }
 }
 
 //Show select event listener
 showSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setShowData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -33,3 +69,6 @@ container.addEventListener('click', e => {
     updateSelectedCount();
   }
 });
+
+//Inital count and total set
+updateSelectedCount();
